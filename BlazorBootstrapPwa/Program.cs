eb5a2fbf8e93f24a06f41b6e0b4633c8f37b7cd7
FrancoisDotNet <56775634+FrancoisDotNet@users.noreply.github.com>
2020-01-22 22:47:03 +0100

    Test

--- BlazorBootstrapPwa/Program.cs
+++ BlazorBootstrapPwa/Program.cs
@@ -9,7 +9,7 @@ namespace BlazorBootstrapPwa
     
         class Startup
         {
-            public void Configure(IComponentsApplicationBuilder b) => b.AddComponent<Body>("body");
+            public void Configure(IComponentsApplicationBuilder b) => b.AddComponent<Body>("ody");
         }
     }
 }
