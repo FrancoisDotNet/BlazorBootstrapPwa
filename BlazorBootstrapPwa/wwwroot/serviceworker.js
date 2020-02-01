d1b54e7767e29c6732ace5faf5c7903c37551432
FrancoisDotNet <56775634+FrancoisDotNet@users.noreply.github.com>
2020-02-01 17:57:45 +0100

    m

--- BlazorBootstrapPwa/wwwroot/serviceworker.js
+++ BlazorBootstrapPwa/wwwroot/serviceworker.js
@@ -197,7 +197,7 @@ var filesToCache = [
     './icons/bootstrap/justify.svg',
     './icons/bootstrap/kanban-fill.svg',
     './icons/bootstrap/kanban.svg',
-    './icons/bootstrap/laptop.svg',
+    /*'./icons/bootstrap/laptop.svg',
     './icons/bootstrap/layout-sidebar-reverse.svg',
     './icons/bootstrap/layout-sidebar.svg',
     './icons/bootstrap/layout-split.svg',
@@ -296,8 +296,8 @@ var filesToCache = [
     './icons/bootstrap/tv-fill.svg',
     './icons/bootstrap/tv.svg',
     './icons/bootstrap/type-bold.svg',
+    './icons/bootstrap/type-h.svg',*/
     './icons/bootstrap/type-h.svg',
-    /*'./icons/bootstrap/type-h.svg',
     './icons/bootstrap/type-h.svg',
     './icons/bootstrap/type-italic.svg',
     './icons/bootstrap/type-strikethrough.svg',
@@ -323,7 +323,7 @@ var filesToCache = [
     './icons/bootstrap/x-octagon.svg',
     './icons/bootstrap/x-square-fill.svg',
     './icons/bootstrap/x-square.svg',
-    './icons/bootstrap/x.svg',*/
+    './icons/bootstrap/x.svg',
     //css
     './css/style.css',
     './css/style.css.map',   
